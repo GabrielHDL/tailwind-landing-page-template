66353e6679663709b84413e4144abc95497e4153
GabrielHDL <gabriel941017@icloud.com>
2024-08-11 00:03:35 -0600

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -200,7 +200,8 @@ export default function Footer({ border = false }: { border?: boolean }) {
 
       {/* Big text */}
       <div className="relative -mt-16 h-60 w-full" aria-hidden="true">
-        <div className="pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-gradient-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['IQonic'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['IQonic'] after:[text-shadow:0_1px_0_white]"></div>
+        <div className="dark:block hidden pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-gradient-to-b before:from-carionixBgDark before:to-carionixDark/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['IQonic'] after:absolute after:inset-0 after:bg-gray-800/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['IQonic'] after:[text-shadow:0_1px_0_rgba(255,255,255,0.08)]"></div>
+        <div className="dark:hidden block pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-gradient-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['IQonic'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['IQonic'] after:[text-shadow:0_1px_0_white]"></div>
         {/* Glow */}
         <div
           className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2/3"
