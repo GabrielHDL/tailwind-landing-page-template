a81b5412c94f5a637803d73f9c6fb24694ce7ae3
GabrielHDL <gabriel941017@icloud.com>
2024-08-10 23:36:35 -0600

    header updated

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
   return (
     <header className="fixed top-2 z-30 w-full md:top-6">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-center gap-3 rounded-2xl dark:bg-carionixDark/40 bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200)] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
+        <div className="relative flex h-14 items-center justify-center gap-3 rounded-2xl dark:bg-carionixDark/70 bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200)] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
           {/* Site branding */}
           <div className="flex items-center">
             <Logo />
